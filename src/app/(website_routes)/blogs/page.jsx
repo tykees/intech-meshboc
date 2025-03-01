de5b73e0f2c4f1229a12831d061ddbcc3b1da50e
tykees <balogun.olu.sam@gmail.com>
2025-03-01 11:31:20 +0100

    mobile view

--- src/app/(website_routes)/blogs/page.jsx
+++ src/app/(website_routes)/blogs/page.jsx
@@ -22,18 +22,18 @@ function Page() {
   }
   return (
     <>
-    <div className='bg-slate-100 h-[500px]'>
+    <div className='bg-slate-100 md:h-[500px] h-[250px] p-3'>
     <div className='pt-20 flex justify-center gap-3 flex-wrap items-center'>
-        <div className='text-[80px] font-bold'>Stay Ahead in </div>
-        <div><Image src={'/phone.gif'} width={60} height={60} alt='gif'></Image></div>
-        <div><Image src={'/balls.png'} width={30} height={30} alt='gif'></Image></div>
-        <div className='text-[80px] font-bold'>Tech</div>
+        <div className='md:text-[80px] text-[20px] font-bold'>Stay Ahead in </div>
+        <div><Image className='md:w-[60px] w-[20px]' src={'/phone.gif'} width={60} height={60} alt='gif'></Image></div>
+        <div><Image  className='md:w-[35px] w-[20px]' src={'/balls.png'} width={30} height={30} alt='gif'></Image></div>
+        <div className='md:text-[80px] text-[20px] font-bold'>Tech</div>
     </div>
     <div className='flex justify-center gap-3 flex-wrap items-center'>
-    <div className='text-[80px] font-bold'>Insights, Trends </div>
-        <div><Image src={'/devabout.gif'} width={60} height={60} alt='gif'></Image></div>
-        <div><Image src={'/inbtw.png'} width={120} height={120} alt='gif'></Image></div>
-        <div className='text-[80px] font-bold'>& Tutorials</div>
+    <div className='md:text-[80px] text-[20px] font-bold'>Insights, Trends </div>
+        <div><Image  className='md:w-[60px] w-[20px]' src={'/devabout.gif'} width={60} height={60} alt='gif'></Image></div>
+        <div><Image  className='md:w-[60px] w-[20px]' src={'/inbtw.png'} width={120} height={120} alt='gif'></Image></div>
+        <div className='md:text-[80px] text-[20px] font-bold'>& Tutorials</div>
     </div>
     <p className='text-center'>Explore expert tips, industry trends, and hands-on guides to boost your tech skills.</p>
     </div>
