c8fbffb4f4965c9e1bdaad35c18c749c85d7345b
tykees <balogun.olu.sam@gmail.com>
2025-06-28 14:58:48 +0100

    cohort

--- src/app/components/Cohort.jsx
+++ src/app/components/Cohort.jsx
@@ -7,10 +7,14 @@ import { Button } from "@/components/ui/button";
 function Cohort() {
   return (
     <div>
-        <div className='bg-black flex gap-1 md:gap-5 py-4 justify-center items-center flex-wrap'>
-               <Image className=' w-[150px] md:w-[100px]' src={"/full2.gif"} width={100} height={100}/>
-            <p className='text-center text-white text-[20px] raleway'>Join Our August 2025 Full Stack Engineering Course</p>
+        <div className='bg-black md:text-left text-center flex gap-1 md:gap-5 py-4 justify-around items-center flex-wrap-reverse'>
+          <div>
+              <p className='text-white text-center md:text-left text-[20px] md:text-[40px] leading-[25px] md:leading-[50px] raleway'>Join Our August 2025 Full Stack <br/> Engineering Course</p>
+              <Image className='w-[150px] my-4 md:w-[150px] m-auto md:m-0' src={"/mern.png"} width={1000} height={1000}/>
              <Link href={"/sign-up"}><Button variant="outline"  className="text-white bg-primary border border-gray-800 poppins w-[150px] h-[50px] hover:text-white hover:border-white">Enrol Now</Button></Link> 
+          </div>
+          
+              <Image className=' w-[450px] md:w-[400px]' src={"/prof2.png"} width={1000} height={1000}/>
         </div>
     </div>
   )
